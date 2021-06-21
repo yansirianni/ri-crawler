b8e8bb646caca62fb96df3543e8a2372943acaa3
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-06-20 23:35:19 -0300

    Melhorias propostas

--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -1,87 +1,82 @@
 from urllib import robotparser
+from urllib.parse import ParseResult
+
 from util.threads import synchronized
 from collections import OrderedDict
 from .domain import Domain
 
-class Scheduler():
-    #tempo (em segundos) entre as requisições
+
+class Scheduler:
+    # tempo (em segundos) entre as requisições
     TIME_LIMIT_BETWEEN_REQUESTS = 20
 
-    def __init__(self,str_usr_agent,int_page_limit,int_depth_limit,arr_urls_seeds):
+    def __init__(self, usr_agent: str, page_limit: int, depth_limit: int, arr_urls_seeds):
         """
-            Inicializa o escalonador. Atributos:
-                - `str_usr_agent`: Nome do `User agent`. Usualmente, é o nome do navegador, em nosso caso,  será o nome do coletor (usualmente, terminado em `bot`)
-                - `int_page_limit`: Número de páginas a serem coletadas
-                - `int_depth_limit`: Profundidade máxima a ser coletada
-                - `int_page_count`: Quantidade de página já coletada
-                - `dic_url_per_domain`: Fila de URLs por domínio (explicado anteriormente)
-                - `set_discovered_urls`: Conjunto de URLs descobertas, ou seja, que foi extraída em algum HTML e já adicionadas na fila - mesmo se já ela foi retirada da fila. A URL armazenada deve ser uma string.
-                - `dic_robots_per_domain`: Dicionário armazenando, para cada domínio, o objeto representando as regras obtidas no `robots.txt`
+        :param usr_agent: Nome do `User agent`. Usualmente, é o nome do navegador, em nosso caso,  será o nome do coletor (usualmente, terminado em `bot`)
+        :param page_limit: Número de páginas a serem coletadas
+        :param depth_limit: Profundidade máxima a ser coletada
+        :param arr_urls_seeds: ?
+
+        Demais atributos:
+        - `page_count`: Quantidade de página já coletada
+        - `dic_url_per_domain`: Fila de URLs por domínio (explicado anteriormente)
+        - `set_discovered_urls`: Conjunto de URLs descobertas, ou seja, que foi extraída em algum HTML e já adicionadas na fila - mesmo se já ela foi retirada da fila. A URL armazenada deve ser uma string.
+        - `dic_robots_per_domain`: Dicionário armazenando, para cada domínio, o objeto representando as regras obtidas no `robots.txt`
         """
-        self.str_usr_agent = str_usr_agent
-        self.int_page_limit = int_page_limit
-        self.int_depth_limit = int_depth_limit
-        self.int_page_count = 0
+        self.usr_agent = usr_agent
+        self.page_limit = page_limit
+        self.depth_limit = depth_limit
+        self.page_count = 0
 
         self.dic_url_per_domain = OrderedDict()
         self.set_discovered_urls = set()
         self.dic_robots_per_domain = {}
 
-
     @synchronized
-    def count_fetched_page(self):
+    def count_fetched_page(self) -> None:
         """
-            Contabiliza o número de paginas já coletadas
+        Contabiliza o número de paginas já coletadas
         """
-        self.int_page_count += 1
+        self.page_count += 1
 
-    def has_finished_crawl(self):
+    def has_finished_crawl(self) -> bool:
         """
-            Verifica se finalizou a coleta
+        :return: True se finalizou a coleta. False caso contrário.
         """
-        if(self.int_page_count > self.int_page_limit):
+        if self.page_count > self.page_limit:
             return True
         return False
 
-
     @synchronized
-    def can_add_page(self,obj_url,int_depth):
+    def can_add_page(self, obj_url: ParseResult, depth: int) -> bool:
         """
-            Retorna verdadeiro caso  profundade for menor que a maxima
-            e a url não foi descoberta ainda
+        :return: True caso a profundidade for menor que a maxima e a url não foi descoberta ainda. False caso contrário.
         """
         return False
 
     @synchronized
-    def add_new_page(self,obj_url,int_depth):
+    def add_new_page(self, obj_url: ParseResult, depth: int) -> bool:
         """
-            Adiciona uma nova página
-            obj_url: Objeto da classe ParseResult com a URL a ser adicionada
-            int_depth: Profundidade na qual foi coletada essa URL
+        Adiciona uma nova página
+        :param obj_url: Objeto da classe ParseResult com a URL a ser adicionada
+        :param depth: Profundidade na qual foi coletada essa URL
+        :return: True caso a página foi adicionada. False caso contrário
         """
-        #https://docs.python.org/3/library/urllib.parse.html
+        # https://docs.python.org/3/library/urllib.parse.html
 
         return False
 
-
-
-
-
-
-
     @synchronized
-    def get_next_url(self):
+    def get_next_url(self) -> tuple:
         """
-        Obtem uma nova URL por meio da fila. Essa URL é removida da fila.
+        Obtém uma nova URL por meio da fila. Essa URL é removida da fila.
         Logo após, caso o servidor não tenha mais URLs, o mesmo também é removido.
         """
-        return None,None
+        return None, None
 
-    def can_fetch_page(self,obj_url):
+    def can_fetch_page(self, obj_url: ParseResult) -> bool:
         """
         Verifica, por meio do robots.txt se uma determinada URL pode ser coletada
         """
-
-
 
         return False
